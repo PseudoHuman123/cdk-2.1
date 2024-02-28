44ae4c90782bbfcd5e9e4dae00ff1bcca52e05bb
PseudoHuman123 <loginid4web2@gmail.com>
2024-02-27 19:23:27 -0500

    Initial commit

---
+++ test/cdk-2.1.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as Cdk21 from '../lib/cdk-2.1-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/cdk-2.1-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new Cdk21.Cdk21Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
